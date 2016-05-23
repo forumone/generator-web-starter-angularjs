4d77b12a28358a47eb9fe7c9656f2fdce44eb38f
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2016-05-23 14:53:54 -0400

    GruntfileEditor

--- web-starter/templates/tasks/register/default.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (grunt) {
-  grunt.registerTask('default', [
-    'build',
-    'connect',
-    // 'simple-watch',
-    'watch' // Must use watch task for LiveReload to work.
-  ]);
-};
